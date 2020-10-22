584b01ed87563cb86a6f64f1e59ad79e2f066b0e
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-10-22 17:28:07 +0200

    :imp: implement movement + pseudo-collision

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Range(-1f, 1f)]
+    public float xMovement;
+    [Range(-1f, 1f)]
+    public float yMovement;
+    private float distanceWall;
+    private float distanceHoleX;
+    private float distanceHoleY;
+    private float velocityX;
+    private float velocityY;
+
+    public float speed = 1.5f;
+
+    public GameObject Wall;
+    public Wall nextWall;
+
+    private void Update() {
+        Vector3 playerPos = transform.position + Vector3.forward * transform.localScale.z;
+        Vector3 wallPos = new Vector3(transform.position.x, transform.position.y, nextWall.transform.position.z) + Vector3.back * nextWall.transform.GetChild(0).transform.localScale.z / 2;
+
+        // If the player reach the wall
+        if (playerPos.z > wallPos.z) {
+            Vector3 freeCellPos = nextWall.cellsPosition[nextWall.indexHole];
+            // If the player is in the free cell
+            if ((freeCellPos - transform.position).sqrMagnitude < (nextWall.transform.GetChild(0).localScale -transform.localScale).sqrMagnitude) {
+                Destroy(nextWall.gameObject);
+                GameObject g = Instantiate(Wall, new Vector3(0, 0, 10), Quaternion.identity);
+                nextWall = g.GetComponent<Wall>();
+                nextWall.player = this;
+            } else
+                Debug.Break();
+        }
+
+        transform.position = transform.position + transform.right * Time.deltaTime * xMovement;
+        transform.position = transform.position + transform.up * Time.deltaTime * yMovement;
+
+        distanceWall = (playerPos - wallPos).magnitude;
+    }
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Wall : MonoBehaviour {
+
+    public GameObject wallTile;
+
+    public float sizeScene = 10;
+    public int nbCellsWall = 16;
+    public PlayerController player;
+
+    public int indexHole;
+    public Dictionary<int, Vector2> cellsPosition;
+
+    Color color;
+    void Start() {
+        int freeX = Random.Range(0, nbCellsWall);
+        int freeY = Random.Range(0, nbCellsWall);
+
+        cellsPosition = new Dictionary<int, Vector2>();
+
+        indexHole = freeX + freeY * nbCellsWall;
+
+        for (int i = 0; i < nbCellsWall; i++) {
+            for (int j = 0; j < nbCellsWall; j++) {
+                int index = i + j * nbCellsWall;
+
+                float x = (sizeScene / nbCellsWall) * i;
+                float y = (sizeScene / nbCellsWall) * j;
+
+                Vector3 cellPos = new Vector3(x - sizeScene / 2, y - sizeScene / 2, transform.position.z);
+
+                cellsPosition.Add(index, cellPos);
+
+                if (i == freeX && j == freeY)
+                    continue;
+
+                GameObject cell = Instantiate(wallTile, cellPos, Quaternion.identity, transform);
+                cell.name = (i + j * nbCellsWall).ToString();
+                cell.transform.localScale = new Vector3(sizeScene / nbCellsWall, sizeScene / nbCellsWall, cell.transform.localScale.z);
+
+            }
+        }
+    }
+
+    void Update() {
+        transform.position = transform.position + Vector3.back * Time.deltaTime * player.speed;
+    }
+}
