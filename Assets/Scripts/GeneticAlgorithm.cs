6201b881d7d1f061b0ceefdb639e107617f76f59
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-10-23 12:33:17 +0200

    multiply list by matrix and feed cells
    (problem with bias value in hidden cells)

--- Assets/Scripts/GeneticAlgorithm.cs
+++ Assets/Scripts/GeneticAlgorithm.cs
@@ -7,9 +7,11 @@ public class GeneticAlgorithm : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float[] inputs = new float[] { 0.3f, 2.4f, 2f, -1f, 1 };
+       // float[] inputs = new float[] { 0.3f, 2.4f, 2f, -1f, 1 };
+        float[] inputs = new float[] { 2f, 10f, 100f, -1f, 1 };
         NeuralNetwork net = new NeuralNetwork(inputs, 2, inputs.Length);
 
+        net.Feed();
         net.DisplayWeights();
     }
 
--- Assets/Scripts/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork.cs
@@ -24,9 +24,9 @@ public class NeuralNetwork
     {
         float[,] weights = new float[sizeI,sizeJ];
 
-        for (int j = 0; j < sizeJ; j++)
+        for (int i = 0; i < sizeI; i++)
         {
-            for (int i = 0; i < sizeI; i++)
+            for (int j = 0; j < sizeJ; j++)
             {
                 weights[i, j] = UnityEngine.Random.Range(0f, 1f);
             }
@@ -35,27 +35,74 @@ public class NeuralNetwork
         return weights;
     }
 
+    float[] MultiplyListMatrix(float[] cells, float[,] weights)
+    {
+        float[,] multWeights = new float[weights.GetLength(0), weights.GetLength(1)];
+        float[] resCells = new float[weights.GetLength(0)];
+
+        for (int j = 0; j < weights.GetLength(1); j++)
+        {
+            for (int i = 0; i < weights.GetLength(0); i++)
+            {
+                Debug.Log(weights.GetLength(1) + " " + cells.Length);
+                multWeights[i, j] = weights[i, j] * cells[j];
+            }
+        }
+
+        for (int i = 0; i < multWeights.GetLength(0); i++)
+        {
+            resCells[i] = 0;
+            for (int j = 0; j < multWeights.GetLength(1); j++)
+            {
+                resCells[i] += multWeights[i, j];
+            }
+        }
+
+        return resCells;
+    }
+
+    float[] ApplyActivationFunction(float[] fedCells)
+    {
+        float[] cellsValues = fedCells;
+
+        for (int i = 0; i < cellsValues.Length; i++)
+        {
+            cellsValues[i] = (float)Math.Tanh(fedCells[i]);
+        }
+
+        return cellsValues;
+    }
+
     public void InitWeights()
     {
-        weightsList.Add(InitLayerWeight(inputs.Length, hidden.Length));
-        weightsList.Add(InitLayerWeight(hidden.Length, outputs.Length));
+        weightsList.Add(InitLayerWeight(hidden.Length - 1, inputs.Length));
+        weightsList.Add(InitLayerWeight(outputs.Length, hidden.Length));
     }
 
     public void Feed()
     {
-
+        hidden = ApplyActivationFunction(MultiplyListMatrix(inputs, weightsList[0]));
+        for (int i = 0; i < hidden.Length; i++)
+        {
+            Debug.Log(hidden[i]);
+        }
+        outputs = ApplyActivationFunction(MultiplyListMatrix(hidden, weightsList[1]));
     }
 
     public void DisplayWeights()
     {
         foreach (float[,] weights in weightsList)
         {
-            string s = "";
-            foreach (float w in weights)
+            for (int i = 0; i < weights.GetLength(0); i++)
             {
-                s += (w + " ");
+                string s = "";
+                for (int j = 0; j < weights.GetLength(1); j++)
+                {
+                    s += (weights[i, j] + " ");
+                }
+                Debug.Log(s);
+                Debug.Log("----");
             }
-            Debug.Log(s);
         }
     }
 }
